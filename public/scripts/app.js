3ae56e87e9d61c78eed79e3626cfadb6b9baf568
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-10-18 15:58:36 -0400

    created function for time elapsed since post

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,56 +1,60 @@
 /* Client-side JS logic goes here
  * jQuery is already loaded */
 
-// Test / driver code (temporary). Eventually will get this from the server.
 
- var data =  
-  [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
+// function to parse the date into amount of time passed
+function parseHumanDate(timeCreated) {
+    var created = new Date(timeCreated);
+    var seconds = Math.floor((Date.now() - created) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+    if (interval >= 1) {
+        if(interval < 2) {
+          return interval + ' year ago';
+        } else {
+          return interval + ' years ago';
+        }
     }
-  ]
-  
+
+    interval = Math.floor(seconds / 2592000);
+    if (interval >= 1) {
+        if(interval < 2) {
+          return interval + ' month ago';
+        } else {
+          return interval + ' months ago';
+        }
+    }
+
+    interval = Math.floor(seconds / 86400);
+    if (interval >= 1) {
+        if(interval < 2) {
+          return interval + ' hour ago';
+        } else {
+          return interval + ' hours ago';
+        }
+    }
+
+    interval = Math.floor(seconds / 3600);
+    if (interval >= 1) {
+        if(interval < 2) {
+          return interval + ' hour ago';
+        } else {
+          return interval + ' hours ago';
+        }
+    }
+
+    interval = Math.floor(seconds / 60);
+    if (interval >= 1) {
+        if(interval < 2) {
+          return interval + ' minute ago';
+        } else {
+          return interval + ' minutes ago';
+        }
+    }
+
+    return Math.floor(seconds) + ' seconds ago';
+}
+
 
 $(document).ready(function() {
     const createTweetElement = function (tweet) {
@@ -65,7 +69,7 @@ $(document).ready(function() {
         $tweet.append($("<p>").text(tweet.content.text));
     
         const $footer = $("<footer>");
-        $footer.append($("<p>").text(new Date(tweet.created_at)));
+        $footer.append($("<p>").text(parseHumanDate(tweet.created_at)));
     
         const $div = $("<div>").attr('id', "hover-content");
         $div.append($("<i class='fa fa-flag' aria-hidden='true'></i>"));
